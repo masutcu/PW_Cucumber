9dce2c2f9303760659c3603cebfbfb27732ef454
Mehmet Ali SÜTÇÜ <119762401+masutcu@users.noreply.github.com>
2024-05-10 20:26:32 +0300

    raport

--- src/corelib/base.spec.ts
+++ src/corelib/base.spec.ts
@@ -1,6 +1,6 @@
 
 import { Before, After, BeforeAll, AfterAll } from "@cucumber/cucumber"
-import { Browser, BrowserContext, Page, chromium } from "playwright"
+import { Browser, BrowserContext, Page, chromium, firefox, webkit } from "playwright"
 import dotenv from "dotenv"
 
 
@@ -22,31 +22,36 @@ BeforeAll(async function () {
             break;
         case 'firefox':
         case 'ff':
-            browser = await chromium.launch({ headless: false, channel: "firefox", args: ['--start-maximized'] })
+            browser = await firefox.launch({ headless: false,  args: ['--start-maximized'] })
+            break;
+        case 'safari':
+            browser = await webkit.launch({ headless: false,  args: ['--start-maximized'] })
             break;
         case 'edge':
         case 'msedge':
             browser = await chromium.launch({ headless: false, channel: "msedge", args: ['--start-maximized'] })
             break;
         default:
-            throw new Error(`invalid browser type ${browserType}  is passed.. pls correct it.. `)
+            throw new Error(`SORUN BURADA: invalid browser type ${browserType}  is passed.. pls correct it.. `)
 
     }
 
 })
 
 
-Before(async () => {
+Before(async  () => {
 
     bCtx = await browser.newContext({ viewport: null, javaScriptEnabled: true })
     page = await bCtx.newPage()
+    
 })
-After(async function () {
+After(async function (scenario) {
     await page.close()
     await bCtx.close()
-
+    console.log(`Scenario Status is  >>>>>>>> ${scenario.result?.status} >>>>>> `)
+   
 })
-
+ 
 AfterAll(async function () {
     await browser.close()
 })
